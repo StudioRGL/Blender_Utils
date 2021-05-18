27d228ca53b9811c006ed9eb9bc66368539ff2aa
df-0 <dave@rgl.tv>
2021-05-18 19:01:13 +0100

    adding settings

--- InitialSetup/setupBlender.py
+++ InitialSetup/setupBlender.py
@@ -1,40 +1,64 @@
 # setup blender
 
+
 import bpy
 
 
 # preinstalled addons to activate
-ACTIVATE_ADDONS = []
+ACTIVATE_ADDONS = ['add_mesh_extra_objects',
+                    'io_scene_fbx',
+                    'space_view3d_modifier_tools',
+                    'mesh_tools',
+                    'mesh_f2',
+                    'mesh_looptools',
+                    'mesh_tiny_cad',
+                    ]  # addons: curve tools, simplify curves+, modifier tools
 
 
 
 def setupBlender():
     print ('setting up blender...')
     # blender startup file set:
-    # 
-    # set cycles mode to GPU
-    # delete objects
-    # color: standard
-    # remove navigation gizmo, relationship lines from all tabs
-    # render_keep user interface
-    # reset 3d cursor
-    # remove stuff from shading view
-    # set outliner filters
-    # add full screen view
-    # play button swap with tool button 
-    # set grease pencil strength/weight
-    # enable Developer Extras
-    # disable splash screen
-    # enable python tooltips
-    # allow negative frames
-    # enable absolute grid snap
-    # 
     
-    
-    # addons: curve tools, simplify curves+, modifier tools
+    # activate addons
     for addon in ACTIVATE_ADDONS:
-        #bpy.ops.preferences.addon_enable(module='')
+        bpy.ops.preferences.addon_enable(module=addon)
         pass
+
+    # setup preferences and shortcuts
+    # https://docs.blender.org/api/current/bpy.types.PreferencesView.html
+    # https://docs.blender.org/api/current/bpy.types.PreferencesSystem.html
+    # https://docs.blender.org/api/current/bpy.types.PreferencesKeymap.html
+    # https://docs.blender.org/api/current/bpy.types.PreferencesInput.html
+    # etc https://docs.blender.org/api/current/bpy.types.html
+
+    prefs = bpy.context.preferences
+    prefs.view.color_picker_type = 'CIRCLE_HSV' # color picker type
+    prefs.view.lookdev_sphere_size = 50 # smaller HDRI spheres
+    prefs.view.show_developer_ui = True # enable Developer Extras
+    prefs.view.show_splash = False # disable splash screen
+    prefs.view.show_statusbar_memory = True
+    prefs.view.show_statusbar_stats = True
+    prefs.view.show_statusbar_version = True
+    prefs.view.show_statusbar_vram = True
+    prefs.view.show_tooltips_python = True    # enable python tooltips
+    # allow negative frames
+    # play button swap with tool button 
+    # render_keep user interface
+    # color: standard
+    # reset 3d cursor
+    
+    # setup default scene
+        # set cycles mode to GPU
+        # delete objects
+        # enable absolute grid snap
+        # remove navigation gizmo, relationship lines from all tabs
+        # remove stuff from shading view
+        # set grease pencil strength/weight
+        # add full screen view
+        # set outliner filters
+    
+    
 
     print('Done!')
 

a438ad51bc2b9d8f5e69ee3d079274836bdc4edf
df-0 <dave@rgl.tv>
2021-05-28 07:28:27 +0100

    added todos

--- InitialSetup/setupBlender.py
+++ InitialSetup/setupBlender.py
@@ -1,5 +1,10 @@
 # setup blender
 
+# TODO: add
+# CyclesPreferences.compute_device_type
+# SWAP KEYFRAME KEYS! up vs down
+# set noodles to curvy
+# 
 
 import bpy
 
@@ -12,6 +17,7 @@ ACTIVATE_ADDONS = ['add_mesh_extra_objects',
                     'mesh_f2',
                     'mesh_looptools',
                     'mesh_tiny_cad',
+                    'rigify'
                     ]  # addons: curve tools, simplify curves+, modifier tools
 
 
@@ -30,9 +36,10 @@ def setupBlender():
     # https://docs.blender.org/api/current/bpy.types.PreferencesSystem.html
     # https://docs.blender.org/api/current/bpy.types.PreferencesKeymap.html
     # https://docs.blender.org/api/current/bpy.types.PreferencesInput.html
-    # etc https://docs.blender.org/api/current/bpy.types.html
+    # etc https://docs.blender.org/api/current/bpy.types.Preferences.html
 
     prefs = bpy.context.preferences
+
     prefs.view.color_picker_type = 'CIRCLE_HSV' # color picker type
     prefs.view.lookdev_sphere_size = 50 # smaller HDRI spheres
     prefs.view.show_developer_ui = True # enable Developer Extras
@@ -42,22 +49,57 @@ def setupBlender():
     prefs.view.show_statusbar_version = True
     prefs.view.show_statusbar_vram = True
     prefs.view.show_tooltips_python = True    # enable python tooltips
-    # allow negative frames
-    # play button swap with tool button 
-    # render_keep user interface
-    # color: standard
-    # reset 3d cursor
+    prefs.view.render_display_type = 'NONE' # render_keep user interface
+
+    prefs.edit.use_negative_frames = True # allow negative frames
+    prefs.edit.undo_steps = 80
+    
+    # sequencer
+    prefs.system.memory_cache_limit = 8192
+    prefs.system.use_sequencer_disk_cache = True
+    prefs.system.sequencer_disk_cache_dir = "D:/Temp/blender/sequencerDiskCache/"
+    prefs.system.sequencer_disk_cache_compression = 'HIGH'
+
+    activeKeyConfig = prefs.keymap.active_keyconfig
+    bpy.context.window_manager.keyconfigs[activeKeyConfig].preferences.spacebar_item = 'TOOL'    # play button swap with tool button https://blender.stackexchange.com/questions/184422/how-to-access-the-kemap-settings-preferences-spacebar-action-select-mouse-butt
+
+    
     
     # setup default scene
-        # set cycles mode to GPU
-        # delete objects
-        # enable absolute grid snap
-        # remove navigation gizmo, relationship lines from all tabs
-        # remove stuff from shading view
-        # set grease pencil strength/weight
-        # add full screen view
-        # set outliner filters
     
+    
+    for screen in bpy.data.screens:
+        for area in screen.areas:
+            if area.type == 'VIEW_3D':
+                s  = area.spaces[0]
+                s.show_gizmo_navigate = False # remove navigation gizmo, relationship lines from all tabs 
+                s.overlay.show_relationship_lines = False
+                s.show_region_toolbar = False
+                s.overlay.show_look_dev = False # remove stuff from shading view
+
+            elif area.type == 'OUTLINER':
+                # set outliner filters
+                s  = area.spaces[0]
+                s.show_restrict_column_render = True
+                s.show_restrict_column_holdout = True
+                s.show_restrict_column_select = True
+                s.show_restrict_column_viewport = True
+
+
+    bpy.context.scene.cycles.device = 'GPU' # set cycles mode to GPU
+    bpy.ops.object.select_all(action = 'SELECT') # select all
+    bpy.ops.object.delete(use_global=False, confirm=False) # delete objects
+    bpy.context.scene.tool_settings.use_snap_grid_absolute = True  # enable absolute grid snap
+    bpy.data.brushes["Pencil"].tool_settings.gpencil_paint.brush.gpencil_settings.pen_strength = 1 # set grease pencil strength/weight
+    
+    # add full screen view
+    bpy.ops.workspace.duplicate()
+    bpy.context.workspace.name = 'Fullscreen'
+
+    #for a in C.workspace.screens[0].areas[1:]:
+
+    
+    bpy.ops.wm.save_homefile() # save new default file
     
 
     print('Done!')

09df513ad1e155f67a7f8f826670cde544bff566
df-0 <dave@rgl.tv>
2021-05-28 09:24:12 +0100

    added to menu

--- Utils/createLayoutNodes.py
+++ Utils/createLayoutNodes.py
@@ -4,19 +4,29 @@ import bpy
 # from https://docs.blender.org/manual/en/latest/advanced/scripting/addon_tutorial.html
 # https://wiki.blender.org/wiki/Process/Addons/Guidelines/metainfo
 bl_info = {
-    "name": "Add Labelled Reroutes",
+    "name": "Add Labelled Reroute Nodes",
     "author": "Studio RGL www.rgl.tv",
-    "description": "A simple addon that adds a right-click option to the node editor menu, 'Add Labelled Reroutes'. This creates labelled reroute nodes for every output of the selected nodes. Keep those node graphs clean!",
+    "description": "A simple addon that adds a right-click option to the node editor menu, 'Add Labelled Reroute Nodes'. This creates labelled reroute nodes for every output of the selected nodes. Keep those graphs clean!",
     "blender": (2, 80, 0),
     "location": "Node Editor > Right Click Menu",
     "category": "Node",
 }
 
+# from https://blender.stackexchange.com/questions/150101/python-how-to-add-items-in-context-menu-in-2-8
+def draw_menu(self, context):
+    layout = self.layout
+    layout.separator()
+    layout.operator("node.duplicate_move", text="Add Labelled Reroute Nodes")
+
+
 def register():
-    print("Hello World")
+    print("Activating addon")
+    bpy.types.NODE_MT_context_menu.append(draw_menu)
+
 
 def unregister():
-    print("Goodbye World")
+    print("Deactivating addon")
+    bpy.types.NODE_MT_context_menu.remove(draw_menu)
 
 
 # create a labelled layout node for each pin on a node

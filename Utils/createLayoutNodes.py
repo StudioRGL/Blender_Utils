e24098330159c7da3475379fec00f76e2a5a9241
df-0 <dave@rgl.tv>
2021-05-28 09:18:31 +0100

    added basic addon info

--- Utils/createLayoutNodes.py
+++ Utils/createLayoutNodes.py
@@ -1,6 +1,25 @@
 import bpy
 
+
+# from https://docs.blender.org/manual/en/latest/advanced/scripting/addon_tutorial.html
+# https://wiki.blender.org/wiki/Process/Addons/Guidelines/metainfo
+bl_info = {
+    "name": "Add Labelled Reroutes",
+    "author": "Studio RGL www.rgl.tv",
+    "description": "A simple addon that adds a right-click option to the node editor menu, 'Add Labelled Reroutes'. This creates labelled reroute nodes for every output of the selected nodes. Keep those node graphs clean!",
+    "blender": (2, 80, 0),
+    "location": "Node Editor > Right Click Menu",
+    "category": "Node",
+}
+
+def register():
+    print("Hello World")
+
+def unregister():
+    print("Goodbye World")
+
+
 # create a labelled layout node for each pin on a node
 def createLabelledReroutes():
     
-    pass
+    pass 
